a936723226ad561b14be231ce1bfe1a425429e9e
felipe-treistman <felipe.treistman@gmail.com>
2023-12-21 16:19:15 -0300

    main fora do diretorio R / batch file

--- main.R
+++ main.R
@@ -3,5 +3,5 @@ library(plumber)
 PORT <- as.numeric(Sys.getenv("PORT", unset = "8080"))
 HOST <- Sys.getenv("HOST", unset = "0.0.0.0")
 
-pr("plumber.R") %>%
+pr("R/plumber.R") %>%
     pr_run(host = HOST, port = PORT, docs = FALSE, quiet = TRUE)
